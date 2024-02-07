697db111b20d2b893aa0adb4d69d1438fdd9a30e
thanasis-tzimas <thanasistzimas@protonmail.com>
2024-02-07 21:16:24 +0200

    Fixed missing classical register from the circuit

--- src/mux.py
+++ src/mux.py
@@ -4,7 +4,7 @@ input = QuantumRegister(2, 'I')
 sel = QuantumRegister(1, 'S')
 anc = QuantumRegister(3, 'anc')
 cr = ClassicalRegister(1, 'c')
-qc = QuantumCircuit(input, sel, anc)
+qc = QuantumCircuit(input, sel, anc, cr)
 
 qc.ccx(input[1], sel, anc[0])
 qc.x(anc[0])
