e7515bcbe5688acf4556d4de19ca2b37d941c17c
thanasis-tzimas <thanasistzimas@protonmail.com>
2024-03-08 18:05:40 +0200

    Fixed missing super()

--- src/library/simple_half_adder.py
+++ src/library/simple_half_adder.py
@@ -11,6 +11,8 @@ class SimpleHalfAdder(QuantumCircuit):
         Args:
             name (str, optional): The name of the circuit object.
         """
+        super().__init__(name=name)
+
         qr_a = QuantumRegister(1, name="a")
         qr_b = QuantumRegister(1, name="b")
         qr_c = QuantumRegister(1, name="cout")
